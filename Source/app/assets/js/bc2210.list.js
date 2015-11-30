2248377b766550d22a2fa78863e1092c0583ef1d
Kuper <rebecca.kuper@finra.org>
2015-11-30 11:03:03 -0500

    moved ie proxy files to better locaiton, added needed js files to prod build grunt.

--- Source/app/assets/js/bc2210.list.js
+++ Source/app/assets/js/bc2210.list.js
@@ -104,52 +104,6 @@
     }
 })();(function() {
     angular.module('listwidget.list')
-        .controller("ScrollController", ScrollController)
-        .directive("preserveScroll", function preserveScroll($window, $timeout){
-            return {
-                restrict: 'AE',
-                controller: 'ScrollController',
-                link: function(scope, element, attr, sctrl) {
-
-                    scope.scrollPos = {};
-
-                    $(element).on("scroll", function() {
-                       // console.log(this.scrollTop);
-                        scope.scrollPos.offset = this.scrollTop;
-                        console.log(scope.scrollPos.offset);
-
-                        sctrl.setPosition(scope.scrollPos.offset);
-                        scope.$apply();
-
-                    });
-
-                }
-            }
-
-        });
-
-    ScrollController.$inject = ['$rootScope', '$window', '$element'];
-    function ScrollController($rootScope, $window, $element) {
-        var scroll = this;
-        scroll.setPosition = setPosition;
-        scroll.getPosition = getPosition;
-        //scroll.offset = 0;
-
-        console.log($element);
-        $element.scrollTop = scroll.getPosition();
-        function getPosition() {
-            return $rootScope.offset;
-        }
-
-        function setPosition(offset) {
-            $rootScope.offset = offset;
-        }
-
-    }
-
-
-})();(function() {
-    angular.module('listwidget.list')
         .controller('ListController', ListController);
 
     ListController.$inject = ['$state',
